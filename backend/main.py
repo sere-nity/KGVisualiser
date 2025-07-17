374a36b5c6b29c271a282072f36acbe9aa9f1931
Serene Liu <serene6197@gmail.com>
2025-07-17 15:45:12 +0100

    feat: upload CSV files into database

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, UploadFile, File
+import csv
+import io
+from fastapi import FastAPI, UploadFile, File, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine, SessionLocal
 from models import CSVUpload, CSVRecord
+from contextlib import asynccontextmanager
+from sqlalchemy.orm import Session
+from datetime import datetime
 
 
-app = FastAPI()
+# ensure tables get created on startup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,7 +25,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Dependency for DB session
+# dependency for DB session
 def get_db():
     db = SessionLocal()
     try:
@@ -22,20 +33,43 @@ def get_db():
     finally:
         db.close()
 
-# endpoints
-
-# Ensure tables get created on startup
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
+# endpoints - uploading CSV, querying?, AI chat?? 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-# uploading CSV, querying?, AI chat?? 
+# has to be synchronous
 @app.post("/upload-csv")
-def upload_csv(file: UploadFile = File(...)):
+def upload_csv(file: UploadFile = File(...), db: Session = Depends(get_db)):
     # process CSV 
-    return {"message": "CSV uploaded successfully"}
+    try:
+        contents = file.file.read().decode("utf-8")  # decode into string from bytes
+        csv_reader = csv.DictReader(io.StringIO(contents)) # io.StringIO - in-memory buffer
+        # save upload metadata to CSVUpload table
+        csv_upload = CSVUpload(
+            filename=file.filename, #Nt - id column is auto-populated
+            created_at=datetime.now()
+        )
+        db.add(csv_upload)
+        db.commit()
+        # refresh to get the auto-generated id for the next table
+        db.refresh(csv_upload)
+
+        # bulk insert each row to CSVRecord table
+        records = []
+        for row in csv_reader:
+            csv_record = CSVRecord(
+                upload_id=csv_upload.id,
+                row_data=row,
+                created_at=datetime.now()
+            )
+            records.append(csv_record)
+        db.add_all(records)
+        db.commit()
+        return {"message": "CSV uploaded successfully", "upload_id": csv_upload.id}
+    
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"CSV upload failed: {e}")
 
